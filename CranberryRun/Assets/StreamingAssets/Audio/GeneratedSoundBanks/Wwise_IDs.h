613d18bd083c0b40f77a3243140b82cb5e3edb7f
vovasamsonov <vovasamsonov42@gmail.com>
2024-01-07 02:53:02 +0300

    Fixed audio bugs

--- CranberryRun/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CranberryRun/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,10 +14,16 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID BALALAIKA = 3322090543U;
+        static const AkUniqueID BEAR = 1190179415U;
         static const AkUniqueID LOST = 221232711U;
         static const AkUniqueID PLAY_MAINMUSICSWITCH = 3684206702U;
+        static const AkUniqueID PLAYERHITOBSTACLE = 3877475578U;
         static const AkUniqueID ROLLING = 4227290872U;
         static const AkUniqueID ROLLING_STOP = 2225439287U;
+        static const AkUniqueID STOP_AMBIENCE = 2477713992U;
+        static const AkUniqueID VODKA = 2902123432U;
+        static const AkUniqueID WALL = 2108779961U;
         static const AkUniqueID WON = 1080430619U;
     } // namespace EVENTS
 
