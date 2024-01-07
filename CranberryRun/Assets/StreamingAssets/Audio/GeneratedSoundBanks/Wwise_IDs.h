31d7626e79918321c51a22a044ee870c70c44444
vovasamsonov <vovasamsonov42@gmail.com>
2024-01-07 13:20:21 +0300

    Fix: - the bullshit

--- CranberryRun/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CranberryRun/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,18 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID BALALAIKA = 3322090543U;
+        static const AkUniqueID BEAR = 1190179415U;
+        static const AkUniqueID LOST = 221232711U;
         static const AkUniqueID PLAY_MAINMUSICSWITCH = 3684206702U;
+        static const AkUniqueID PLAYERHITOBSTACLE = 3877475578U;
+        static const AkUniqueID ROLLING = 4227290872U;
+        static const AkUniqueID ROLLING_STOP = 2225439287U;
+        static const AkUniqueID STOP_AMBIENCE = 2477713992U;
+        static const AkUniqueID VODKA = 2902123432U;
+        static const AkUniqueID WALL = 2108779961U;
+        static const AkUniqueID WON = 1080430619U;
     } // namespace EVENTS
 
     namespace STATES
@@ -43,7 +54,40 @@ namespace AK
             } // namespace STATE
         } // namespace MUSICSTATE
 
+        namespace PLAYERSTATE
+        {
+            static const AkUniqueID GROUP = 3285234865U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PLAYERALIVE = 2557321869U;
+                static const AkUniqueID PLAYERDEAD = 2356585300U;
+            } // namespace STATE
+        } // namespace PLAYERSTATE
+
     } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace PLAYER_SPEED
+        {
+            static const AkUniqueID GROUP = 1062779386U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FAST = 2965380179U;
+                static const AkUniqueID MEDIUM = 2849147824U;
+                static const AkUniqueID SLOW = 787604482U;
+            } // namespace SWITCH
+        } // namespace PLAYER_SPEED
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_PLAYERSPEED = 2653406601U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
