5cc864e26d0565284f2bebe5b26337e9aba18b9a
RomanT <r.tarasyuta@chillgaming.net>
2024-01-08 00:22:56 +0700

    modelSpawner

--- CranberryRun/Assets/ModelSpawner.cs
+++ CranberryRun/Assets/ModelSpawner.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ModelSpawner : MonoBehaviour
 {
@@ -26,24 +26,20 @@ public class ModelSpawner : MonoBehaviour
         _endPointZ = _endPoint.transform.position.z;
         _distance = _endPointZ - _startPointZ;
 
-        int modelsCount = _modelsToSpawn.Count;
-
-        var step = _distance / modelsCount;
+        var step = _distance / 2f;
 
         var tmpList = new List<GameObject>(_modelsToSpawn);
 
         var currentPoint = _startPointZ;
 
-        for (int i = 0; i < _modelsToSpawn.Count; i++)
+        for (int i = 0; i < step; i++)
         {
             var pickUpModel = tmpList[Random.Range(0, tmpList.Count)];
-            tmpList.Remove(pickUpModel);
             Vector3 pointToInstantiate = new Vector3(_startPoint.transform.position.x, _startPoint.transform.position.y,
                 currentPoint);
 
             Instantiate(pickUpModel, pointToInstantiate, Quaternion.identity);
-            currentPoint += step;
+            currentPoint += 2f;
         }
     }
-
 }
