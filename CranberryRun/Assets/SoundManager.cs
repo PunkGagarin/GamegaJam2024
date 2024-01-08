ddfd4ab05a34e022c7983d1315619eee086e0afc
RomanT <r.tarasyuta@chillgaming.net>
2024-01-08 21:54:55 +0700

    mainMenu sound slider

--- CranberryRun/Assets/SoundManager.cs
+++ CranberryRun/Assets/SoundManager.cs
@@ -5,6 +5,18 @@ public class SoundManager : MonoBehaviour
 {
 
     [SerializeField]
+    private Sprite _muteSprite;
+
+    [SerializeField]
+    private Sprite _lowHalfSprite;
+
+    [SerializeField]
+    private Sprite _highHalfSprite;
+
+    [SerializeField]
+    private Image _sliderImage;
+
+    [SerializeField]
     private Slider _slider;
 
     private void Awake()
@@ -15,5 +27,11 @@ public class SoundManager : MonoBehaviour
     private void ChangeMasterVolume(float sliderValue)
     {
         AkSoundEngine.SetRTPCValue("MasterVolume", sliderValue);
+        _sliderImage.sprite = sliderValue switch
+        {
+            <= 0 => _muteSprite,
+            <= 0.5f => _lowHalfSprite,
+            _ => _highHalfSprite
+        };
     }
 }
